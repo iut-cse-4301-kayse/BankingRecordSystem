d3b91443fecb9a1e816198a6006b2b4049e4e436
iut-cse-4301-kayse <39086397+iut-cse-4301-kayse@users.noreply.github.com>
2018-05-15 22:06:01 +0600

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<fstream>
+#include<cstdlib>
+using std::cout;
+using std::cin;
+using std::endl;
+using std::fstream;
+using std::ofstream;
+using std::ifstream;
+using std::ios;
+class account_query
+{
+private:
+    char account_number[20];
+    char firstName[10];
+    char lastName[10];
+    float total_Balance;
+public:
+    void read_data();
+    void show_data();
+    void write_rec();
+    void read_rec();
+    void search_rec();
+    void edit_rec();
+    void delete_rec();
+};
+void account_query::read_data()
+{
+    cout<<"\nEnter Account Number: ";
+    cin>>account_number;
+    cout<<"Enter First Name: ";
+    cin>>firstName;
+    cout<<"Enter Last Name: ";
+    cin>>lastName;
+    cout<<"Enter Balance: ";
+    cin>>total_Balance;
+    cout<<endl;
+}
+void account_query::show_data()
+{
+    cout<<"Account Number: "<<account_number<<endl;
+    cout<<"First Name: "<<firstName<<endl;
+    cout<<"Last Name: "<<lastName<<endl;
+    cout<<"Current Balance: Rs.  "<<total_Balance<<endl;
+    cout<<"-------------------------------"<<endl;
+}
